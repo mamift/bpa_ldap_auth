268ce5dc7118153390541b5716905cf5cd94f4a3
Muhammad Miftah <muhammad.miftah@bpanz.com>
2017-03-10 13:05:25 +1000

    Basically implemented all the required functionaliy. Wow that was simple, why did I take so long?

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,64 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+/**
+ * Ensures that the username and password of a basic HTTP authentication header are not empty.
+ * @param Request $request
+ * @param Response $response
+ * @param callable $next
+ * @return mixed
+ */
+$empty_auth_fields = function(Request $request, Response $response, callable $next): Response {
+    $username = $request->getHeader('PHP_AUTH_USER')[0];
+    $password = $request->getHeader('PHP_AUTH_PW')[0];
+
+    if (empty($password)) {
+        return $response->withJson(json_message_array("The password cannot be empty!", false));
+    }
+
+    if (empty($username)) {
+        return $response->withJson(json_message_array("The username cannot be empty!", false));
+    }
+
+    $next_response = $next($request, $response);
+
+    return $next_response;
+};
+
+/**
+ * Allow CORS from a specific domain.
+ * @param Request $request
+ * @param Response $response
+ * @param callable $next
+ * @return mixed
+ */
+$specific_CORS = function(Request $request, Response $response, callable $next) {
+    $route = $request->getAttribute("route");
+
+    $methods = [];
+
+    if (!empty($route)) {
+        $pattern = $route->getPattern();
+
+        foreach ($this->router->getRoutes() as $route) {
+            if ($pattern === $route->getPattern()) {
+                $methods = array_merge_recursive($methods, $route->getMethods());
+            }
+        }
+        // methods holds all of the HTTP Verbs that a particular route handles.
+    } else {
+        $methods[] = $request->getMethod();
+    }
+
+    $response = $next($request, $response);
+    $response = $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods))
+        ->withHeader("Access-Control-Allow-Origin", "*"); // TODO: need to change to specific domain for the dashboard!!
+    return $response;
+};
+
+$app->add($empty_auth_fields);
+$app->add($specific_CORS);
--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,68 @@
 <?php
 // Routes
+use Slim\Http\Request;
+use Slim\Http\Response;
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+/**
+ * Generates an array for use in JSON responses. Should really be used for status messages.
+ * @param string $message
+ * @param bool $status
+ * @param array|null $data
+ * @return array
+ */
+function json_message_array(string $message, bool $status = true, ?array $data = []): array {
+    $return_data = [
+        'success' => $status,
+        'message' => $message,
+        'data' => $data
+    ];
+
+    if (empty($data) || $data == null) {
+        unset($return_data['data']);
+    }
+
+    return $return_data;
+}
+
+/**
+ * Binds to the dirty BPANZ server with a username and password.
+ * @param string $username [username must include the appropriate NetBIOS prefix, cannot be empty or null]
+ * @param string $password [the password cannot be empty or null]
+ * @return bool [true if binding succeeded]
+ * @throws Exception
+ */
+function bind_to_dirty_bpanz_domain(string $username, string $password): bool {
+    if (empty($username) || empty($password)) {
+        // this should never occur as there's middleware that will check for empty usernames or passwords in the
+        // Authorization header
+        throw new Exception("Empty username or password!");
+    }
+    $dirty_bpanz = ldap_connect('ldap://bpa-d-server01.bpanz.local');
+    return ldap_bind($dirty_bpanz, $username, $password);
+}
+
+/**
+ * The login route. Will accept only basic HTTP authentication
+ */
+$app->get('/login', function(Request $request, Response $response) {
+
+    $username = $request->getHeader('PHP_AUTH_USER')[0];
+    $password = $request->getHeader('PHP_AUTH_PW')[0];
+
+    $is_authenticated = bind_to_dirty_bpanz_domain($username, $password);
+
+    $message = ($is_authenticated === false ? "Unsuccessful authentication" : "Successful authentication");
+    $data = json_message_array($message, $is_authenticated);
+
+    $new_response = $response->withJson($data);
+    return $new_response;
+});
+
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
